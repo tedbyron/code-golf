72a949b4785cec966d1931a091e937af82c233fa
Theodore Byron <ted@tedbyron.com>
2018-06-19 22:29:17 -0400

    Update sum_of_two_lowest_positive_integers.js

--- sum_of_two_lowest_positive_integers.js
+++ sum_of_two_lowest_positive_integers.js
@@ -13,7 +13,7 @@ s=n=>n.sort((x,y)=>x-y).slice(0,2).reduce((x,y)=>x+y)
  * @param {number[]} numbers - input array
  */
 function s(n) {
-  return n.sort(function(x, y) { return x - y })    // sort n with compareFunction that compares each element to the next with subtraction (smaller integers have smaller compare values, sorts integers 
+  return n.sort(function(x, y) { return x - y })    // sort n with compareFunction that compares each element to the next with subtraction (sorts ascending)
           .slice(0,2)                               // slice first two elements from sorted array                            
           .reduce(function(x, y) { return x + y })  // reduce two-element array with callback that sums each element
 }
