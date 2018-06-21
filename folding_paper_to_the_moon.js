4a4cc79a53034707b0fb2872d6588e06822c7ee8
Theodore Byron <ted@tedbyron.com>
2018-06-20 23:31:52 -0400

    Update folding_paper_to_the_moon.js

--- folding_paper_to_the_moon.js
+++ folding_paper_to_the_moon.js
@@ -10,7 +10,7 @@ f=d=>d>0?Math.max(0,Math.ceil(Math.log2(1e4*d))):null
 function foldTo(d) {
   return d > 0
     ? Math.max(0, Math.ceil( // required positive integer return
-      Math.log2(1e4 * d)) // approximate folds based on 2^n/.0001 = d -> 2^n = 1000d
+      Math.log2(1e4 * d)) // calculates folds based on .0001*2^n = d -> 2^n = 1000d
     )
     : null; // required null return for negative input
 };
