dc82064362d7f81158be33863f4a03c44ad01c96
Theodore Byron <ted@tedbyron.com>
2018-06-27 01:24:15 -0400

    Add time_to_future_money_value.js

---
+++ 2018_js/time_to_future_money_value.js
@@ -0,0 +1,25 @@
+/**
+ * Calculate the years to reach a future sum of money given a present value of
+ * money, a yearly compounding interest rate, a yearly income tax rate, and the
+ * desired future value.
+ */
+
+/**
+ * golfed
+ */
+calculateYears=(p,r,t,f)=>{for(i=0;p<f;++i)p+=p*r*(1-t);return i}
+
+/**
+ * ungolfed
+ * @param  {number} p     present value
+ * @param  {number} r     interest rate
+ * @param  {number} t     tax rate
+ * @param  {number} f     future value
+ * @return {number}       number of years for p to reach f
+ */
+calculateYears = (p, r, t, f) => {
+  for (i = 0; p < f; ++i) {
+    p += p * r * (1 - t) // simplified time value of money equation with tax
+  }
+  return i
+}
