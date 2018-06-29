b2d6f5205f9f63b335b7f04e3a71185ff6b5a8cb
Theodore Byron <ted@tedbyron.com>
2018-06-28 22:25:19 -0400

    Rename folder, add dna_complements.js

---
+++ 2018/dna_complements.js
@@ -0,0 +1,30 @@
+/**
+ * In DNA strings, symbols "A" and "T" are complements of each other, as "C"
+ * and "G". You have function with one side of the DNA (string, except for
+ * Haskell); you need to get the other complementary side. The DNA strand is
+ * never empty or there is no DNA at all
+ */
+
+/**
+ * golfed
+ */
+d=a=>a.replace(/./g,c=>b[c])
+b={A:'T',T:'A',C:'G',G:'C'}
+
+
+/**
+ * ungolfed
+ * @param {string} a the input DNA nucleobases
+ */
+function dnaStrand(a) => {
+  return a.replace(/./g, c => { // replace every character with the value in b
+    return b[c]
+  })
+}
+
+b = {
+  A: 'T',
+  T: 'A',
+  C: 'G',
+  G: 'C'
+}
