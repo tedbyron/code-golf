f4bf750c78bfe870b580bbd426b9a9a216f3088f
Theodore Byron <ted@tedbyron.com>
2018-07-10 22:49:37 -0400

    Add knuth_up_arrow_notation.js

---
+++ 2018/knuth_up_arrow_notation.js
@@ -0,0 +1,23 @@
+/**
+ * Knuth up-arrow notation
+ */
+
+/**
+ * golfed
+ */
+f=(a,n,b)=>n>1||b>0?a**b:f(a,n-1,f(a,n,b-1))
+
+/**
+ * ungolfed
+ * @param  {number} a base of the function
+ * @param  {number} n number of up-arrows
+ * @param  {number} b 'power' of the function
+ * @return {number}   result of the function
+ */
+const knuth = (a, n, b) => {
+  return b !== 0
+    ? n !== 1
+      ? knuth(a, n - 1, knuth(a, n, b - 1))
+      : a ** b
+    : 1;
+};
