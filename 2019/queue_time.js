a8902383bf5410634b1e5a2e55c0d10222114166
Theodore Byron <ted@tedbyron.com>
2019-04-23 16:31:54 -0400

    Add queue_time.js

---
+++ 2019/queue_time.js
@@ -0,0 +1,25 @@
+/**
+ * "There is a queue for the self-checkout tills at the supermarket. Your task
+ * is write a function to calculate the total time required for all the
+ * customers to check out. The function parameters are an array representing
+ * the time it will take each customer to check out and the number of checkout
+ * tills."
+ */
+
+/**
+ * golfed
+ */
+f=(a,b)=>Math.max(...a.reduce((c,d)=>{c[c.indexOf(Math.min(...c))]+=d;return c},Array(b).fill(0)))
+
+/**
+ * ungolfed
+ * @param   {number[]} customers times it will take each customer to check out
+ * @param   {number}   n         the number of checkout tills
+ * @returns {number}             the time required for all customers to checkout
+ */
+function queueTime(customers, n) {
+  return Math.max(...customers.reduce((a, c) => {
+    a[a.indexOf(Math.min(...a))] += c;
+    return a;
+  }, Array(n).fill(0)));
+}
