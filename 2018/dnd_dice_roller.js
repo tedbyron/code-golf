6cf4fe06d373cf0d15458047fb94192b5be0fc0c
Theodore Byron <ted@tedbyron.com>
2019-04-24 00:41:43 -0400

    Update dnd_dice_roller.js

--- 2018/dnd_dice_roller.js
+++ 2018/dnd_dice_roller.js
@@ -9,18 +9,18 @@
 f=a=>{[b,c,e=[]]=a.split('d').map(x=>+x);for(;b>0;)e[--b]=(Math.random()*c+1)|0;return`${e.reduce((x,y)=>x+y)}: ${e.join(' ')}`}
 
 /**
- * ungolfed
- * @param   {string} a an input string
- * @returns {string}   the sum of rolls and a list of the rolls
+ * rolls a Y sided dice X times
+ * @param   {string} input a string in the format 'XdY'
+ * @returns {string}       the sum of rolls and a list of the rolls
  */
-function dndRoll(a) {
-  const [B, C] = a.split('d').map(x => parseInt(x));
-  let e = [];
+function dndRoll(input) {
+  const [TIMES, SIDES] = input.split('d').map(cur => parseInt(cur));
+  const rolls = [];
 
-  for (let i = 0; i < B; i++) {
-    e[i] = Math.floor(Math.random() * C + 1);
+  for (let i = 0; i < TIMES; i++) {
+    rolls.push(Math.floor(Math.random() * SIDES + 1));
   }
 
-  let sum = e.reduce((x, y) => x + y);
-  return `${sum}: ${e.join(' ')}`;
+  const SUM = rolls.reduce((acc, cur) => acc + cur);
+  return `${SUM}: ${rolls.join(' ')}`;
 }
