0f82d8985d1c332986fa6e16c91a8c2a806aaeba
Theodore Byron <ted@tedbyron.com>
2019-05-01 07:09:01 -0400

    Update multiplication_table.js

--- js/2019/multiplication_tables.js
+++ js/2019/multiplication_tables.js
@@ -1,11 +1,25 @@
-multiplicationTable=(a,b)=>{
-  let r = Array(a).fill([...Array(b).keys()].map(c=>c+1))
-  for (let i = 0; i < a; i++) {
-    for (let j of r[i]) {
-      j = (i + 1) * j
-      console.log(j)
-    }
-  }
+/**
+ * "Create a function that accepts dimensions, of Rows x Columns, as parameters
+ * in order to create a multiplication table sized according to the given
+ * dimensions. The return value of the function must be an array, and the
+ * numbers must be Fixnums, NOT strings."
+ */
 
-  return r
+/**
+ * golfed function
+ */
+f=(a,b)=>[...Array(a)].map((_,i)=>[...Array(b)].map((_,j)=>(i+1)*(j+1)))
+
+/**
+ * ungolfed
+ * @param   {number}     numRows    the number of rows the table should have
+ * @param   {number}     numColumns the number of columns the table should have
+ * @returns {number[][]}            the resulting multiplication table
+ */
+function multiplicationTable(numRows, numColumns) {
+  return [...Array(numRows)].map((_, i) => {
+    return [...Array(numColumns)].map((_, j) => {
+      return (i + 1) * (j + 1);
+    });
+  });
 }
