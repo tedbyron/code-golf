83ae273c1d0d15655c12553c214f417d3205a942
Theodore Byron <ted@tedbyron.com>
2019-05-05 21:13:01 -0400

    Update caesar_cipher_variant_1.js

--- js/2019/caesar_cipher_variant_1.js
+++ js/2019/caesar_cipher_variant_1.js
@@ -1,15 +1,34 @@
 /**
- * @param   {string} str
- * @param   {number} key
- * @returns {string}
+ * @param   {string}   str string to shift
+ * @param   {number}   key how many characters to shift the first character
+ * @returns {string[]}     array of 5 strings with each character shifted by
+ *                         key + index
  */
 function movingShift(str, key) {
-  let arr = [...str];
+  const arr = [...str];
+
   return arr.map((cur, idx) => {
-    return cur.replace(/[a-z]/gi, m => {
+    return cur.replace(/[a-z]/i, m => {
       const charCode = m.charCodeAt();
       const letterCase = charCode & 96;
+
       return String.fromCharCode((charCode - letterCase + key + idx + 129) % 26 + letterCase + 1);
     });
   }).join('').match(RegExp(`.{1,${Math.ceil(arr.length/5)}}`, 'g'));
+}
+
+/**
+ * @param   {string[]} arr array of 5 strings that have been shifted
+ * @param   {number}   key how many characters the first character was shifted
+ * @returns {string}       joined arr with each character shifted back
+ */
+function demovingShift(arr, key) {
+  return [...arr.join('')].map((cur, idx) => {
+    return cur.replace(/[a-z]/i, m => {
+      const charCode = m.charCodeAt();
+      const letterCase = charCode & 96;
+
+      return String.fromCharCode((charCode - letterCase - key - idx + 129) % 26 + letterCase + 1);
+    });
+  }).join('');
 }
