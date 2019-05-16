ab4e630204250fba0fad62b84b9303c0ea6e2974
Theodore Byron <ted@tedbyron.com>
2019-05-16 16:45:42 -0400

    Update caesar_cipher_variant_1.js
    add golfed functions and description

--- js/2019/caesar_cipher_variant_1.js
+++ js/2019/caesar_cipher_variant_1.js
@@ -1,4 +1,22 @@
 /**
+ * "This program performs a variation of the Caesar shift. The shift increases
+ * by 1 for each character (on each iteration).If the shift is initially 1, the
+ * first character of the message to be encoded will be shifted by 1, the second
+ * character will be shifted by 2, etc."
+ */
+
+/**
+ * golfed cipher function
+ */
+x=(a,b,c=[...a],d=c.map((e,f)=>e.replace(/[a-z]/i,g=>String.fromCharCode((h=g.charCodeAt(),i=h&96,h-i+b+f+129)%26-~i))).join``.match(RegExp(`.{1,${Math.ceil(c.length/5)}}`,'g')))=>d.length^5?d.concat(['']):d
+
+/**
+ * golfed decipher function
+ */
+y=(a,b)=>[...a.join``].map((c,d)=>c.replace(/[a-z]/i,e=>String.fromCharCode((f=e.charCodeAt(),g=f&96,f-g-b-d+129)%26-~g))).join``
+
+/**
+ * ungolfed cipher function
  * @param   {string}   str string to shift
  * @param   {number}   key how many characters to shift the first character
  * @returns {string[]}     array of 5 strings with each character shifted by key
@@ -20,6 +38,7 @@ function movingShift(str, key) {
 }
 
 /**
+ * ungolfed decipher function
  * @param   {string[]} arr array of shifted characters
  * @param   {number}   key how many characters the first character is shifted
  * @returns {string}       arr reverse shifted and joined
