34a93376b51e67b29dee5dc49550881a121b7432
Theodore Byron <ted@tedbyron.com>
2019-05-16 15:42:07 -0400

    Update caesar_cipher_variant_1.js

--- js/2019/caesar_cipher_variant_1.js
+++ js/2019/caesar_cipher_variant_1.js
@@ -1,26 +1,28 @@
 /**
  * @param   {string}   str string to shift
  * @param   {number}   key how many characters to shift the first character
- * @returns {string[]}     array of 5 strings with each character shifted by
- *                         key + index
+ * @returns {string[]}     array of 5 strings with each character shifted by key
+ *                         + index
  */
 function movingShift(str, key) {
-  const arr = [...str];
+  const splitStr = [...str];
 
-  return arr.map((cur, idx) => {
+  const arr = splitStr.map((cur, idx) => {
     return cur.replace(/[a-z]/i, m => {
       const charCode = m.charCodeAt();
       const letterCase = charCode & 96;
 
       return String.fromCharCode((charCode - letterCase + key + idx + 129) % 26 + letterCase + 1);
     });
-  }).join('').match(RegExp(`.{1,${Math.ceil(arr.length/5)}}`, 'g'));
+  }).join('').match(RegExp(`.{1,${Math.ceil(splitStr.length/5)}}`, 'g'));
+
+  return arr.length === 5 ? arr : arr.concat(['']);
 }
 
 /**
- * @param   {string[]} arr array of 5 strings that have been shifted
- * @param   {number}   key how many characters the first character was shifted
- * @returns {string}       joined arr with each character shifted back
+ * @param   {string[]} arr array of shifted characters
+ * @param   {number}   key how many characters the first character is shifted
+ * @returns {string}       arr reverse shifted and joined
  */
 function demovingShift(arr, key) {
   return [...arr.join('')].map((cur, idx) => {
